19ceeb5f8b8d2cccdd251e24ba1a078ee0f0642d
aramsay03 <alan@aramsay.co.uk>
2020-04-09 12:28:30 +0100

    Initial Commit with models created

---
+++ src/main/java/com/codeclam/example/coursebookinglab/CoursebookinglabApplication.java
@@ -0,0 +1,13 @@
+package com.codeclam.example.coursebookinglab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursebookinglabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursebookinglabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclam/example/coursebookinglab/CoursebookinglabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclam.example.coursebookinglab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursebookinglabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
